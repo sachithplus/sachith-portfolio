709c851cd1a2f7a058aa914eee2ea14eb2b642a3
Sachith Waidyasekara <sachith.plus@gmail.com>
2025-06-13 09:32:40 -0700

    Nav bar and Hamburger Menu
    Feat: Implement responsive navigation with hamburger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+    const menuLinks = document.querySelector('.menu-links');
+
+    hamburgerIcon.classList.toggle('open');
+    menuLinks.classList.toggle('open');
+}
